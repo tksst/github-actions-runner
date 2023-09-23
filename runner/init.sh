e4ab176f8ae42e091510e5944e3b18a6b90c836c
Takashi Sato <takashi@tks.st>
2023-09-23 19:29:42 +0900

    fix: container does not exit correctly

--- runner/init.sh
+++ runner/init.sh
@@ -1,10 +1,22 @@
-#!/bin/dash
+#!/bin/bash
 
 set -e
 set -u
 
 ./config.sh  --unattended --name "$( date '+%Y%m%dT%H%M%S%z' )" "$@"
 
-trap './config.sh remove --token "${TOKEN}"' EXIT
+# get token from "--token XXXX"
+while [ $# -gt 0 ]; do
+  case $1 in
+    --token) TOKEN=$2; break;;
+	*) shift ;;
+  esac
+done
 
-./run.sh
+# dash needs this to run the EXIT handler when it receives SIGTERM/INT/HUP.
+trap 'echo "init.sh: Received signal, terminating the runner"; kill -TERM $pid' INT TERM HUP
+trap 'echo "init.sh: Removing this runner from GitHub"; ./config.sh remove --token "${TOKEN}"' EXIT
+
+./run.sh &
+pid=$!
+wait -f $pid
